c5df9ee956d1cec991149fe7b72d3be1a8e164d3
SynI20N <leonid.shcherbakov.01@mail.ru>
2024-02-12 23:05:06 +0300

    solved 3rd task

--- lab1/task3.c
+++ lab1/task3.c
@@ -6,12 +6,23 @@
 
 typedef struct {
     int color;
+    GtkWidget* label;
 } color;
 
 typedef void (*callback)(GtkWidget* widget, gpointer data);
 
 static void change_color(GtkWidget* widget, gpointer data) {
-    
+    color* c = (color*)data;
+    if(c->color) {
+        gtk_widget_remove_css_class(GTK_WIDGET(c->label), "red-background");
+        gtk_widget_add_css_class(GTK_WIDGET(c->label), "green-background");
+        c->color = 0;
+    }
+    else {
+        gtk_widget_add_css_class(GTK_WIDGET(c->label), "red-background");
+        gtk_widget_remove_css_class(GTK_WIDGET(c->label), "green-background");
+        c->color = 1;
+    }
 }
 
 void set_event(const char* s, GtkWidget* widget, color* l, callback func) {
@@ -21,22 +32,27 @@ void set_event(const char* s, GtkWidget* widget, color* l, callback func) {
 }
 
 static void activate(GtkApplication *app, gpointer user_data) {
+    GtkBuilder *builder = gtk_builder_new();
+    gtk_builder_add_from_file(builder, "task3.ui", NULL);
     GdkDisplay *display = gdk_display_get_default ();
     GtkCssProvider *provider = gtk_css_provider_new ();
-    gtk_css_provider_load_from_resource (provider, "styles.css");
+    GObject *window = gtk_builder_get_object(builder, "window");
+    GObject *label = gtk_builder_get_object(builder, "label");
+    gtk_css_provider_load_from_path(provider, "styles.css");
     gtk_style_context_add_provider_for_display (display,
                                                 GTK_STYLE_PROVIDER (provider),
                                                 GTK_STYLE_PROVIDER_PRIORITY_FALLBACK);
-    GtkBuilder *builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "task3.ui", NULL);
-    GObject *window = gtk_builder_get_object(builder, "window");
-    //gtk_widget_add_css_class (GTK_WIDGET(window), "error");
     gtk_window_set_application(GTK_WINDOW(window), app);
     gtk_window_set_default_size(GTK_WINDOW(window), 300, 300);
+    gtk_widget_add_css_class(GTK_WIDGET(label), "red-background");
+    color* color = malloc(sizeof(*color));
+    color->color = 1;
+    color->label = GTK_WIDGET(label);
+    set_event("motion", GTK_WIDGET(label), color, change_color);
 
-    gtk_widget_set_visible(GTK_WIDGET(window), TRUE);
+    gtk_widget_show(GTK_WIDGET(window));
 
-    g_object_unref (provider);
+    g_object_unref(provider);
     g_object_unref(builder);
 }
 
