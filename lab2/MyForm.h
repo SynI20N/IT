c3a27a17ea8a941b09e86dc1f50a0c15fc005975
SynI20N <leonid.shcherbakov.01@mail.ru>
2024-02-28 23:38:53 +0300

    added drive update event + images

--- lab2/MyForm.h
+++ lab2/MyForm.h
@@ -41,7 +41,15 @@ namespace FileSystem {
 			}
 		}
 	private: System::Windows::Forms::TextBox^ textBox1;
-	protected:
+	protected: virtual void WndProc(System::Windows::Forms::Message% m) override
+	{
+		//Do stuff in here
+		Form::WndProc(m);
+		if (m.Msg == WM_DEVICECHANGE)
+		{
+			Show_Drives();
+		}
+	}
 
 	protected:
 	private:
@@ -49,26 +57,24 @@ namespace FileSystem {
 		//static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 	protected:
 
-	protected:
-
 	private:
 		/// <summary>
 		/// ќб€зательна€ переменна€ конструктора.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
-		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте
 		/// содержимое этого метода с помощью редактора кода.
 		/// </summary>
 		void InitializeComponent(void)
 		{
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
-			// 
+			//
 			// textBox1
-			// 
+			//
 			this->textBox1->Enabled = false;
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
@@ -77,9 +83,9 @@ namespace FileSystem {
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(704, 214);
 			this->textBox1->TabIndex = 0;
-			// 
+			//
 			// MyForm
-			// 
+			//
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(729, 261);
@@ -94,10 +100,11 @@ namespace FileSystem {
 
 #pragma endregion
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
-		Show_Drives(sender, e);
+		Show_Drives();
 	}
-	private: System::Void Show_Drives(System::Object^ sender, System::EventArgs^ e) 
+	private: System::Void Show_Drives()
 	{
+		textBox1->Text = "";
 		int length = 1000;
 		CHAR* output = (CHAR*)malloc(sizeof(*output) * length);
 		DWORD size = GetLogicalDriveStringsA(
@@ -106,7 +113,7 @@ namespace FileSystem {
 		);
 		const int count = 10;
 		CHAR drives[count][200] = { 0 };
-		strcpy(drives[0],"Disk\tTotal Space, Gb\tFree Space, Gb\0");
+		strcpy(drives[0], "Disk\tDrive Type\t\tTotal Space, Gb\tFree Space, Gb\0");
 		int j = 0, k = 1;
 		for (int i = 0; i < size; i++)
 		{
@@ -134,16 +141,52 @@ namespace FileSystem {
 				continue;
 			}
 			BOOL res = GetDiskFreeSpaceExA(drives[i], &freespace, &totalnumbtype, &totalnumberfreebyte);
+			UINT type = GetDriveTypeA(drives[i]);
+			std::string driveType = Type2Str(type);
 			std::string ws(drives[i]);
 			String^ str = gcnew String(ws.c_str());
+			String^ strT = gcnew String(driveType.c_str());
 			double total = totalnumbtype.QuadPart / pow(1024, 3);
 			double totalFree = totalnumberfreebyte.QuadPart / pow(1024, 3);
-			textBox1->AppendText(str + "\t" + total +  "\t" + totalFree + L"\r\n");
+			String^ row = String::Format("{0,-15} {1,-38} {2,-25} {3,-25}\r\n", str, strT, total.ToString("f4"), totalFree.ToString("f4"));
+			textBox1->AppendText(row);
 		}
+	}
+	private: std::string Type2Str(UINT diskType)
+	{
+		std::string res;
+		switch (diskType)
+		{
+		case 0:
+			res = "DRIVE_UNKNOWN";
+			break;
+		case 1:
+			res = "DRIVE_NO_ROOT_DIR";
+			break;
+		case 2:
+			res = "DRIVE_REMOVABLE";
+			break;
+		case 3:
+			res = "DRIVE_FIXED";
+			break;
+		case 4:
+			res = "DRIVE_REMOTE";
+			break;
+		case 5:
+			res = "DRIVE_CDROM";
+			break;
+		case 6:
+			res = "DRIVE_RAMDISK";
+			break;
+		default:
+			res = "NO_TYPE";
+			break;
+		}
+		return res;
 	}
 	private: System::Void listView1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
-};
+	};
 }
