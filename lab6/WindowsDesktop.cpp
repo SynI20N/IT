479c3f493ef1ba3b36b4d26f253f37eee0186f17
SynI20N <leonid.shcherbakov.01@mail.ru>
2024-04-18 12:39:33 +0300

    lab6 tried using plain winapi

---
+++ lab6/WindowsDesktop.cpp
@@ -0,0 +1,191 @@
+// HelloWindowsDesktop.cpp
+// compile with: /D_UNICODE /DUNICODE /DWIN32 /D_WINDOWS /c
+
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+#include <iostream>
+
+// Global variables
+
+// The main window class name.
+static TCHAR szWindowClass[] = _T("DesktopApp");
+
+// The string that appears in the application's title bar.
+static TCHAR szTitle[] = _T("Windows");
+
+// Stored instance handle for use in Win32 API calls such as FindResource
+HINSTANCE hInst;
+HWND handle;
+int enumPtr;
+HDC drawHdc;
+
+// Forward declarations of functions included in this code module:
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK EnumWindowsProc(_In_ HWND   hWnd, _In_ LPARAM lParam);
+VOID CALLBACK Wineventproc(
+    HWINEVENTHOOK hWinEventHook,
+    DWORD event,
+    HWND hwnd,
+    LONG idObject,
+    LONG idChild,
+    DWORD idEventThread,
+    DWORD dwmsEventTime
+);
+
+int WINAPI WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR     lpCmdLine,
+    _In_ int       nCmdShow
+)
+{
+    WNDCLASSEX wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wcex))
+    {
+        MessageBox(NULL,
+            _T("Call to RegisterClassEx failed!"),
+            _T("Windows Desktop Guided Tour"),
+            NULL);
+
+        return 1;
+    }
+
+    // Store instance handle in our global variable
+    hInst = hInstance;
+
+    // The parameters to CreateWindowEx explained:
+    // WS_EX_OVERLAPPEDWINDOW : An optional extended window style.
+    // szWindowClass: the name of the application
+    // szTitle: the text that appears in the title bar
+    // WS_OVERLAPPEDWINDOW: the type of window to create
+    // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
+    // 500, 100: initial size (width, length)
+    // NULL: the parent of this window
+    // NULL: this application does not have a menu bar
+    // hInstance: the first parameter from WinMain
+    // NULL: not used in this application
+    HWND hWnd = CreateWindowEx(
+        WS_EX_OVERLAPPEDWINDOW,
+        szWindowClass,
+        szTitle,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        500, 800,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    if (!hWnd)
+    {
+        MessageBox(NULL,
+            _T("Call to CreateWindow failed!"),
+            _T("Windows Desktop Guided Tour"),
+            NULL);
+
+        return 1;
+    }
+    SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, NULL, Wineventproc, 0, 0, WINEVENT_OUTOFCONTEXT);
+    handle = hWnd;
+
+    // The parameters to ShowWindow explained:
+    // hWnd: the value returned from CreateWindow
+    // nCmdShow: the fourth parameter from WinMain
+    ShowWindow(hWnd,
+        nCmdShow);
+    UpdateWindow(hWnd);
+    //EnumWindows(EnumWindowsProc, 0);
+
+    // Main message loop:
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+VOID CALLBACK Wineventproc(
+    HWINEVENTHOOK hWinEventHook,
+    DWORD event,
+    HWND hwnd,
+    LONG idObject,
+    LONG idChild,
+    DWORD idEventThread,
+    DWORD dwmsEventTime
+)
+{
+    SendMessage(
+        handle,
+        WM_PAINT,
+        NULL,
+        NULL
+    );
+}
+
+BOOL CALLBACK EnumWindowsProc(_In_ HWND   hWnd,_In_ LPARAM lParam)
+{
+    int length = GetWindowTextLength(hWnd);
+    if (length > 0 && enumPtr < 20) {
+        wchar_t* name = (wchar_t*)malloc(sizeof(name) * (length+1));
+        GetWindowText(hWnd, name, length+1);
+        enumPtr++;
+        RECT rect = { 40, 10 + 30*enumPtr };
+        SetTextColor(drawHdc, 0x00000000);
+        SetBkMode(drawHdc, TRANSPARENT);
+        DrawText(drawHdc, name, -1, &rect, DT_SINGLELINE | DT_NOCLIP);
+    }
+    return true;
+}
+
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+static HWND hwndEdit;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+
+    switch (message)
+    {
+    case WM_PAINT:
+        hdc = BeginPaint(handle, &ps);
+        drawHdc = hdc;
+        enumPtr = 0;
+        EnumWindows(EnumWindowsProc, 0);
+
+        EndPaint(handle, &ps);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+        break;
+    }
+
+    return 0;
+}
---
+++ lab6_sharp/MainForm.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lab8Windows
+{
+	public partial class MainForm : Form
+	{
+		// Добавляем хук для отслеживания изменений активного окна
+		private delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+		private const uint EVENT_SYSTEM_FOREGROUND = 0x0003;
+		private const uint WINEVENT_OUTOFCONTEXT = 0;
+		private static WinEventDelegate procDelegate = new WinEventDelegate(WinEventProc);
+		private static IntPtr hhook = IntPtr.Zero;
+
+		public MainForm()
+		{
+			InitializeComponent();
+			EnumerateWindows();
+
+			// Устанавливаем хук
+			SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, procDelegate, 0, 0, WINEVENT_OUTOFCONTEXT);
+		
+			// Заполняем ComboBox при запуске формы
+            PopulateComboBox();
+		}
+
+		public void EnumerateWindows()
+		{
+			// Очищаем текстовое поле перед обновлением
+			textBox1.Clear();
+
+			EnumWindows((hWnd, lParam) => {
+				int length = GetWindowTextLength(hWnd);
+				if (length > 0) {
+					StringBuilder sb = new StringBuilder(length + 1);
+					GetWindowText(hWnd, sb, sb.Capacity);
+					string windowTitle = sb.ToString();
+					textBox1.AppendText(windowTitle + Environment.NewLine);
+				}
+				return true;
+			}, IntPtr.Zero);
+		}
+
+		// Метод для заполнения ComboBox списком всех открытых окон
+        void PopulateComboBox()
+        {
+        	// Получаем список окон других процессов
+	        List<IntPtr> windows = GetOtherProcessWindows();
+	
+	        // Очищаем ComboBox1 и добавляем заголовки окон
+	        comboBox1.Items.Clear();
+	        foreach (IntPtr hWnd in windows)
+	        {
+	            string windowTitle = GetWindowTitle(hWnd);
+	            comboBox1.Items.Add(windowTitle);
+	        }
+        }
+        
+		// Метод обработки события изменения активного окна
+		static void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+		{
+			// Получаем главную форму
+			MainForm mainForm = null;
+			foreach (Form form in Application.OpenForms) {
+				if (form is MainForm) {
+					mainForm = (MainForm)form;
+					break;
+				}
+			}
+			if (mainForm != null) {
+				// Вызываем метод обновления списка окон
+				mainForm.EnumerateWindows();
+			}
+		}
+
+		[DllImport("user32.dll")]
+		static extern IntPtr GetForegroundWindow();
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		static extern int GetWindowTextLength(IntPtr hWnd);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+		private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+		[DllImport("user32.dll", EntryPoint = "GetClassLongPtr")]
+		static extern IntPtr GetClassLongPtr(IntPtr hWnd, int nIndex);
+
+		[DllImport("shell32.dll", CharSet = CharSet.Auto)]
+		static extern uint ExtractIconEx(string lpszFile, int nIconIndex, out IntPtr phiconLarge, IntPtr phiconSmall, uint nIcons);
+
+		// Добавляем хук WinEvent
+		[DllImport("user32.dll")]
+		static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+		[DllImport("user32.dll")]
+		static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+		
+		[DllImport("user32.dll", SetLastError = true)]
+		static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        
+		// Освобождаем хук при закрытии формы
+		protected override void OnFormClosed(FormClosedEventArgs e)
+		{
+			base.OnFormClosed(e);
+			UnhookWinEvent(hhook);
+		}
+        
+		void TextBox1TextChanged(object sender, EventArgs e) {}
+		
+		void ResizeClick(object sender, EventArgs e)
+		{
+			// Получаем дескриптор текущего окна
+			IntPtr handle = this.Handle;
+
+			// Увеличиваем размеры окна на 100 пикселей по ширине и высоте
+			SetWindowPos(handle, IntPtr.Zero, 0, 0, Int32.Parse(this.textBox2.Text) + 100, Int32.Parse(this.textBox3.Text) + 100, SWP_NOMOVE | SWP_NOZORDER);
+		}
+		
+		void FormStarButtonClick(object sender, EventArgs e)
+		{
+			try {
+				// Получаем выбранный элемент ComboBox1
+		    	var selectedItem = comboBox1.SelectedItem as string;
+				if (selectedItem != null)
+				{
+				    // Получаем дескриптор окна
+				    IntPtr handle = FindWindow(null, selectedItem);
+				
+				    // Проверяем, что окно было найдено
+				    if (handle != IntPtr.Zero)
+				    {
+				        // Изменяем стиль окна на "WS_EX_DLGMODALFRAME"
+				        SetWindowLong(handle, GWL_EXSTYLE, WS_EX_DLGMODALFRAME);
+				
+				        // Перерисовываем окно с новым стилем
+				        SetWindowPos(handle, IntPtr.Zero, 0, 0, 0, 0, SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER);
+				    }
+				    else
+				    {
+				        MessageBox.Show("Окно не найдено.");
+				    }
+				}
+			}
+			
+			catch (NullReferenceException) {
+				MessageBox.Show("Ой", "NullReferenceException");
+			}
+		}
+		
+		List<IntPtr> GetOtherProcessWindows()
+	    {
+	        List<IntPtr> windows = new List<IntPtr>();
+	        EnumWindows((hWnd, lParam) =>
+	        {
+	            int length = GetWindowTextLength(hWnd);
+	            if (length > 0)
+	            {
+	                windows.Add(hWnd);
+	            }
+	            return true;
+	        }, IntPtr.Zero);
+	        return windows;
+	    }
+
+	    string GetWindowTitle(IntPtr hWnd)
+	    {
+	        int length = GetWindowTextLength(hWnd);
+	        if (length > 0)
+	        {
+	            StringBuilder sb = new StringBuilder(length + 1);
+	            GetWindowText(hWnd, sb, sb.Capacity);
+	            return sb.ToString();
+	        }
+	        return string.Empty;
+	    }
+
+		[DllImport("user32.dll")]
+		static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+		
+		[DllImport("user32.dll")]
+    	static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+		const uint SWP_NOMOVE = 0x0002;
+		const uint SWP_NOZORDER = 0x0004;
+		const int GWL_EXSTYLE = -20;
+	    const int WS_EX_DLGMODALFRAME = 0x00000001;
+	    const uint SWP_FRAMECHANGED = 0x0020;
+	    const uint SWP_NOSIZE = 0x0001;
+	}
+}
